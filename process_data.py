68c6323ff1ce258ed1a5fe06a65c19505b3df21d
Philipp M. Wolf <philipp.wolf@physics.uu.se>
2021-02-25 18:48:35 +0100

    Started to write a script to analyze the Titanic data

---
+++ process_data.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 25 16:54:12 2021
+
+@author: phili
+"""
+# import pandas as pd
+
+def process_train(data):
+    
+    #Fill empty age and fare with the mean of each
+    data['Age'] = data['Age'].fillna(data['Age'].mean())
+    data['Fare'] = data['Fare'].fillna(data['Fare'].mean())
+    
+    #Fill missing Embarked value with most common which is S
+    data['Embarked'] = data['Embarked'].fillna('S')
+    
+    #Drop Name, Ticket and Cabin since they will not be used in my first try.
+    data.drop('Name', axis=1, inplace=True)
+    data.drop('Ticket', axis=1, inplace=True)
+    data.drop('Cabin', axis=1, inplace=True)
+    
+    #changing sex into number, male = 1 and female = 0
+    data['Sex'] = data['Sex'].map({'female':0, 'male':1})
+    
+    #changing Embarked into number, S = 0, C = 1 and Q = 2
+    data['Embarked'] = data['Embarked'].map({'S':0, 'C':1, 'Q':2})
+    return data
+    
